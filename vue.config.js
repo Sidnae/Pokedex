a89aeb93c820248d7d4869978269ad47159e6463
Sandie Skeen <sidnae.sk@gmail.com>
2022-08-19 13:53:49 +0200

    corrected vue config

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-//const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
-const publicPath = '/pokedex/'
-module.exports = {
- publicPath: publicPath,
-}
